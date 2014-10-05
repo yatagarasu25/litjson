b775e69481d17007806bee83685c34b896cfa810
Igor Karatayev <igor.cwer@gmail.com>
2014-10-06 00:22:47 +0300

    Stream serializer helpers.

---
+++ src/LitJson/JsonSerializerEx.cs
@@ -0,0 +1,20 @@
+﻿using LitJson;
+using System.IO;
+
+namespace LitJson
+{
+	public static class JsonSerializerEx
+	{
+		public static void Serialize<T>(Stream stream, T o)
+		{
+			using (var s = new StreamWriter(stream))
+				s.Write(JsonMapper.ToJson(o));
+		}
+
+		public static T Deserialize<T>(Stream stream)
+		{
+			using (var s = new StreamReader(stream))
+				return JsonMapper.ToObject<T>(s.ReadToEnd());
+		}
+	}
+}
